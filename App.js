7576a41ac1e4dde6df424221772bcac529d42fdc
Ahrarhussain <68767564+Ahrarhussain@users.noreply.github.com>
2023-03-26 10:40:48 +0530

    Download dependencies!

---
+++ App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+import {Box,Text} from 'grommet';
+
+function App() {
+  return (
+    <Box>
+      <Text>Working Fine!</Text>
+    </Box>
+  );
+}
+
+export default App;
