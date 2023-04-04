c0adc64ab45608d931e52e6d992b14d161a721c0
Abrar Ahmed <abbu0503@gmail.com>
2023-04-04 18:28:52 +0530

    Added pages and components to be rendered with instructions as to where. Logic has to be added

---
+++ pawm/src/Components/AssetDetailsPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function AssetDetailsPage(){
+    return(
+        // this page has to show to add, update asset details
+        <div>Sample text</div>
+    );
+}
+
+export default AssetDetailsPage;
