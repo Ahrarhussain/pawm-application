67ccb437e8fbe089f43bae03f5538ad25c17a569
Abrar Ahmed <66716543+abrar0503@users.noreply.github.com>
2023-04-07 10:49:07 +0530

    Updates firebase db name

--- pawm/src/Services/asset.services.js
+++ pawm/src/Services/asset.services.js
@@ -9,20 +9,20 @@ import {
     doc,
 } from 'firebase/firestore';
 
-const assetCollectionRef = collection(db, "pawm");
+const assetCollectionRef = collection(db, "assets");
 class AssetDataService {
     
-    addAssets = (newAsset) => {
+    addAsset = (newAsset) => {
         return addDoc(assetCollectionRef, newAsset);
     };
 
     updateAsset = (id, updatedAsset) => {
-        const assetDoc = doc(db, "pawm", id);
+        const assetDoc = doc(db, "aseets", id);
         return updateDoc(assetDoc, updatedAsset);
     };
 
     deleteAsset = (id) => {
-        const assetDoc = doc(db, "pawm", id);
+        const assetDoc = doc(db, "assets", id);
         return deleteDoc(assetDoc);
     };
 
@@ -30,8 +30,8 @@ class AssetDataService {
         return getDocs(assetCollectionRef);
     }
 
-    getBook = (id) => {
-        const assetDoc = doc(db, "pawm", id);
+    getAsset = (id) => {
+        const assetDoc = doc(db, "assets", id);
         return getDoc(assetDoc);
     }
 }
